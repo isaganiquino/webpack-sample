eb3cb59869839c9ea9006aa8b56df50bbe6f78e5
isaganiquino <isagani.quino@gmail.com>
2020-09-04 21:10:13 +0800

    commit assessment webpack config changes

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# webpack-sample
-Webpack assessment
+# webpack-assessment
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = function () {
+  return {
+    entry: {
+      main: './src/js/index.js',
+      vendor: ['./src/js/vendors/vendor1.js', './src/js/vendors/vendor2.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: '[name].[contentHash].bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+      }),
+    ],
+  };
+};
